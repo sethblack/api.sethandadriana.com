cd9d21a6e5dc2d72a1177103d1642af5a9257523
Seth Black <sblack@sethserver.com>
2016-10-23 12:39:51 -0500

    handle flipped images

--- snaapi/snaapi/models.py
+++ snaapi/snaapi/models.py
@@ -11,6 +11,24 @@ import re
 
 logger = logging.getLogger(__name__)
 
+def flip_horizontal(im): return im.transpose(Image.FLIP_LEFT_RIGHT)
+def flip_vertical(im): return im.transpose(Image.FLIP_TOP_BOTTOM)
+def rotate_180(im): return im.transpose(Image.ROTATE_180)
+def rotate_90(im): return im.transpose(Image.ROTATE_90)
+def rotate_270(im): return im.transpose(Image.ROTATE_270)
+def transpose(im): return rotate_90(flip_horizontal(im))
+def transverse(im): return rotate_90(flip_vertical(im))
+orientation_funcs = [None,
+                 lambda x: x,
+                 flip_horizontal,
+                 rotate_180,
+                 flip_vertical,
+                 transpose,
+                 rotate_270,
+                 transverse,
+                 rotate_90
+                ]
+
 class WeddingPicture(models.Model):
     owner = models.CharField(max_length=64)
     upload_date = models.DateTimeField(auto_now_add=True)
@@ -41,11 +59,9 @@ class WeddingPicture(models.Model):
         except:
             return False
 
-        # max width = 600
-        # max height = 500
-        # image.thumbnail((600,500), Image.ANTIALIAS)
-
         width, height = image.size
+
+        image = apply_orientation(image)
 
         if width > height:
             image.thumbnail((10000,500), Image.ANTIALIAS)
@@ -82,6 +98,30 @@ class WeddingPicture(models.Model):
         temp_thumb.close()
 
         return True
+
+    def apply_orientation(self, im):
+        """
+        Extract the oritentation EXIF tag from the image, which should be a PIL Image instance,
+        and if there is an orientation tag that would rotate the image, apply that rotation to
+        the Image instance given to do an in-place rotation.
+
+        :param Image im: Image instance to inspect
+        :return: A possibly transposed image instance
+        """
+
+        try:
+            kOrientationEXIFTag = 0x0112
+            if hasattr(im, '_getexif'): # only present in JPEGs
+                e = im._getexif()       # returns None if no EXIF data
+                if e is not None:
+                    #log.info('EXIF data found: %r', e)
+                    orientation = e[kOrientationEXIFTag]
+                    f = orientation_funcs[orientation]
+                    return f(im)
+        except:
+            # We'd be here with an invalid orientation value or some random error?
+            pass # log.exception("Error applying EXIF Orientation tag")
+        return im
 
     def save(self, *args, **kwargs):
         super(WeddingPicture, self).save(*args, **kwargs)
