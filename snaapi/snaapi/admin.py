1f207a954d958e3f6723d510c9aa564720bb98fb
Seth Black <sblack@sethserver.com>
2016-10-25 21:04:12 -0500

    update admin system

--- snaapi/snaapi/admin.py
+++ snaapi/snaapi/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from snaapi import models
 
-admin.site.register(models.UploadCode)
-admin.site.register(models.WeddingPicture)
+
+class UploadCodeAdmin(admin.ModelAdmin):
+    model = models.UploadCode
+    list_display = ('code', 'full_name', )
+
+
+class WeddingPictureAdmin(admin.ModelAdmin):
+    model = models.WeddingPicture
+    list_display = ('owner', 'upload_date', 'picture', )
+
+
+admin.site.register(models.UploadCode, UploadCodeAdmin)
+admin.site.register(models.WeddingPicture, WeddingPictureAdmin)
